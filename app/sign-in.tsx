7ab2b80876b6ab4a3c228429afe191622edff918
Janitha Senani Karunarathna <janithakarunarathna12@gmail.com>
2025-01-11 12:02:01 +0530

    implement the method to login in the ui

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,13 +1,20 @@
-import {View, Text, SafeAreaView, Animated, ScrollView, Image, TouchableOpacity} from 'react-native'
+import {View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity, Alert} from 'react-native'
 import React from 'react'
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import {login} from "@/lib/appwrite";
 
 
 const SignIn = () => {
 
-    const handleLogin = () => {
+    const handleLogin = async () => {
+        const result = await login();
 
+        if(result){
+            console.log('Login Success');
+        } else {
+            Alert.alert('Error',"Failed to Login");
+        }
     }
 
     return (
